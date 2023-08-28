77232756bbbffc27d53badeca048a33c98577e5c
yeyupiaoling <yeyupiaoling@foxmail.com>
2023-08-28 22:26:29 +0800

    更新版本

--- mvits/__init__.py
+++ mvits/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 # 支持语言
 LANGUAGE_MARKS = dict()
 LANGUAGE_MARKS['cjke_cleaners2'] = {"普通话": "[ZH]", "日本語": "[JA]", "English": "[EN]", "한국어": "[KO]", "Mix": ""}
